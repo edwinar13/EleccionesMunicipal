978e1db8d0febc8b04665bce415c3a0415668675
edwinar13 <edwinarevalo88@gmail.com>
2023-09-22 23:52:20 -0500

    v.0.05

--- src/static/js/scripts.js
+++ src/static/js/scripts.js
@@ -1,68 +1,107 @@
+const shieldRosal = document.getElementById('shield-rosal');
+const shieldContainer = document.getElementById('shield-container');
+const centroX = shieldContainer.offsetWidth / 2;
+const centroY = shieldContainer.offsetHeight / 2;
+let autoRotate = true;
 
-const imagen = document.getElementById('escudo_rosal');
-const centroX = imagen.offsetWidth / 2;
-const centroY = imagen.offsetHeight / 2;
+shieldContainer.addEventListener('mousemove', function (event) {
 
-imagen.addEventListener('mousemove', function(event) {
-    const mouseX = event.clientX - imagen.getBoundingClientRect().left;
-    const mouseY = event.clientY - imagen.getBoundingClientRect().top;
+    const mouseX = event.clientX - shieldContainer.getBoundingClientRect().left;
+    const mouseY = event.clientY - shieldContainer.getBoundingClientRect().top;
 
     const deltaX = (mouseX - centroX) / centroX;
     const deltaY = (mouseY - centroY) / centroY;
 
     // Aplica la transformación 3D basada en la posición del mouse
-    imagen.style.transform = `rotateX(${-30 * deltaY}deg) rotateY(${30 * deltaX}deg)`;
+    shieldRosal.style.transform = `rotateX(${-30 * deltaY}deg) rotateY(${30 * deltaX}deg)`;
 });
 
-imagen.addEventListener('mouseleave', function() {
-    // Reinicia la transformación cuando el mouse sale de la imagen
-    imagen.style.transform = 'rotateX(0deg) rotateY(0deg)';
+shieldContainer.addEventListener('mouseenter', function () {
+    autoRotate = false; // Detener la rotación automática al colocar el mouse sobre la imagen
 });
+
+shieldContainer.addEventListener('mouseleave', function () {
+    autoRotate = true; // Reanudar la rotación automática al quitar el mouse de la imagen
+});
+
+
+
+
+
+let currentIndex = 0;
+const rotations = [
+    { x: 7, y: 7 },
+    { x: 0, y: 10 },
+    { x: -7, y: 7 },
+    { x: -10, y: 0 },
+    { x: -7, y: -7 },
+    { x: 0, y: -10 },
+    { x: 7, y: -7 },
+    { x: 10, y: 0 },
+];
+
+function animate() {
+    if (autoRotate) {
+        const rotation = rotations[currentIndex];
+        shieldRosal.style.transform = `rotateX(${rotation.x}deg) rotateY(${rotation.y}deg)`;
+        currentIndex = (currentIndex + 1) % rotations.length;
+        shieldRosal.style.transition = `transform ${3}.0s`;
+    } else {
+        shieldRosal.style.transition = `transform ${0}.8s`;
+    }
+}
+
+setInterval(animate, 1000);
+
+
+
+
+
 
 
 function updateCountdown() {
     const now = new Date().getTime();
     const targetDate = new Date("2023-09-30T12:00:00").getTime(); // Fecha objetivo
     const timeRemaining = targetDate - now;
-  
+
     const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
     const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
-  
+
     updateCard("days-card", days);
     updateCard("hours-card", hours);
     updateCard("minutes-card", minutes);
     updateCard("seconds-card", seconds);
-  }
-  
-  function updateCard(cardId, newValue) {
+}
+
+function updateCard(cardId, newValue) {
     const card = document.getElementById(cardId);
     const currentValue = card.querySelector("span").textContent;
-  
+
     if (currentValue !== newValue.toString()) {
-      // Cambia el valor y aplica el efecto de giro solo si es diferente
-      card.style.transform = "rotateX(45deg)";
-      setTimeout(() => {
-        card.style.transform = "rotateX(0deg)";
-        card.querySelector("span").textContent = newValue;
-      }, 500); // Duración de la animación en milisegundos
+        // Cambia el valor y aplica el efecto de giro solo si es diferente
+        card.style.transform = "rotateX(45deg)";
+        setTimeout(() => {
+            card.style.transform = "rotateX(0deg)";
+            card.querySelector("span").textContent = newValue;
+        }, 500); // Duración de la animación en milisegundos
     }
-  }
-  
-  // Actualizar el contador cada segundo
-  setInterval(updateCountdown, 1000);
-  
-  // Inicializar el contador
-  updateCountdown();
-  
-  
-  $(document).ready(function() {
+}
+
+// Actualizar el contador cada segundo
+setInterval(updateCountdown, 1000);
+
+// Inicializar el contador
+updateCountdown();
+
+
+$(document).ready(function () {
     $("#poll-form button").prop("disabled", false);
     $(".loader").hide();
 
     // Manejar la respuesta de éxito o error al hacer una solicitud AJAX
-    $("#poll-form").on("submit", function(event) {
+    $("#poll-form").on("submit", function (event) {
         event.preventDefault(); // Evitar que el formulario se envíe de forma predeterminada
         $("#poll-form button").prop("disabled", true);
         $(".loader").show();
@@ -71,7 +110,7 @@ function updateCountdown() {
             url: "/enviar_correo", // URL a la que se envía la solicitud
             method: "POST", // Método de la solicitud
             data: $(this).serialize(), // Datos del formulario serializados            
-            success: function(response) {
+            success: function (response) {
                 // Manejar la respuesta exitosa si es necesario
                 // Por ejemplo, si deseas redirigir a otra página:
                 // window.location.href = "/index"; 
@@ -84,25 +123,25 @@ function updateCountdown() {
                     $("#email-user").val("");
                     $("#poll-form button").prop("disabled", false);
                     $(".loader").hide();
-                    setTimeout(function() {
+                    setTimeout(function () {
                         // Ocultar la alerta
                         $("#alert-message").addClass("alert-hidden");
                     }, 6000);
                 }
             },
-            error: function(jqXHR) {
+            error: function (jqXHR) {
                 if (jqXHR.status === 400 || jqXHR.status === 500) {
                     // Aplicar el estilo de alerta de error
                     $("#alert-message").removeClass("alert-success alert-hidden").addClass("alert-error");
                     $("#alert-message").text(jqXHR.responseJSON.message);
                     $("#alert-message").show();
-                    
+
                     // Habilitar el botón y ocultar el span de carga
                     $("#poll-form button").prop("disabled", false);
                     $(".loader").hide();
 
                     //$("#email-user").val("");
-                    setTimeout(function() {
+                    setTimeout(function () {
                         // Ocultar la alerta
                         $("#alert-message").addClass("alert-hidden");
                     }, 6000);
