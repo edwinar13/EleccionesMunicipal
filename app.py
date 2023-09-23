91f01beeb6c617e4b3ab0391ba141be98aa8c93f
edwinar13 <edwinarevalo88@gmail.com>
2023-09-23 09:10:39 -0500

    v0.08

--- app.py
+++ app.py
@@ -3,4 +3,5 @@ from decouple import config
 
 if __name__ == '__main__':
     app.debug = config('DEBUG', default=False, cast=bool)
-    app.run(host='0.0.0.0', port=5000)
+    port = config('PORT', default=5000, cast=int)  # Cargar el valor de PORT como entero
+    app.run(host='0.0.0.0', port=port)  # Usar el valor de port como el puerto de la aplicación
